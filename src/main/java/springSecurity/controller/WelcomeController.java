1023cbd2b5669f4d8048c2455309d5b72d7b7838
Larissa Fernandes <l.fernandes0308@gmail.com>
2024-07-11 16:28:58 -0300

    commit do projeto completo

--- src/main/java/springSecurity/controller/WelcomeController.java
+++ src/main/java/springSecurity/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package springSecurity;
+package springSecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
